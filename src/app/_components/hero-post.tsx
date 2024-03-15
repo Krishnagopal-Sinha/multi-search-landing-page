b65f549ace76afd490015a1d9cc2644b29c5454a
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-03-15 11:29:20 +0530

    added read more link

--- src/app/_components/hero-post.tsx
+++ src/app/_components/hero-post.tsx
@@ -42,8 +42,17 @@ export function HeroPost({
           </div>
         </div>
         <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          <Avatar name='KrishnaGopal Sinha' picture={author.picture} />
+          <p className="text-lg leading-relaxed mb-4">
+            {excerpt}{" "}
+            <Link
+              as={`/posts/${slug}`}
+              href="/posts/[slug]"
+              className="hover:underline italic"
+            >
+              (Read more...)
+            </Link>
+          </p>
+          <Avatar name="KrishnaGopal Sinha" picture={author.picture} />
         </div>
       </div>
     </section>
