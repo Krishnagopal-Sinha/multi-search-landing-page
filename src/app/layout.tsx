e591f7968a85223949188edd7abf46fe0122b686
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-03-20 22:51:57 +0530

    updated title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Multi Search NewTab`,
+  title: `🔍 Multi Search NewTab`,
   description: `The Ultimate Search Accelerator for Your Browser!`,
 };
 
