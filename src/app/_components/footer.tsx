01c9b7528fe542e23e424a226e1be52fae48ef55
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-03-20 22:49:06 +0530

    button text changes

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -11,19 +11,19 @@ export function Footer() {
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="https://chromewebstore.google.com/detail/multi-search-newtab/oiiajjjedfdfboacleknggbcbkjhngen"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0 text-center"
             >
               Chrome Web Store
             </a>
             <a
               href="https://microsoftedge.microsoft.com/addons/detail/multi-search-newtab/ngdmedfpcnmlkgoeglkdicbidhnncmad"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0 text-center"
             >
               Edge Add-ons Store
             </a>
             <a
               href="https://addons.mozilla.org/en-US/firefox/addon/multi-search-newtab-multi-tab/"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-6 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-6 duration-200 transition-colors mb-6 lg:mb-0 text-center"
             >
               Firefox Add-ons Store
             </a>
